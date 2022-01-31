8e8c15165099dabb96f722a8bea3bfbcff82e508
kevin-guzman <kevinskate.kg@gmail.com>
2022-01-31 15:39:25 -0500

    feat(): go modules

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go_intermediate
 
 go 1.17
+
+require github.com/donvito/hellomod v1.0.1
+
+require github.com/donvito/hellomod/v2 v2.0.0
--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"github.com/donvito/hellomod"
+	hellomod2 "github.com/donvito/hellomod/v2"
 )
 
-func sum(values ...int) int {
-	total := 0
-	for _, number := range values {
-		total += number
-	}
-	return total
-}
-
-func printNames(names ...string) {
-	for _, name := range names {
-		fmt.Println(name)
-	}
-}
-
-func getValues(x int) (double int, triple int, cuadruple int) {
-	double = 2 * x
-	triple = 3 * x
-	cuadruple = 4 * x
-	return
-}
-
 func main() {
-	fmt.Println(sum(1, 2, 1, 1))
-	printNames("Kevin", "Manolo", "Juanita", "Ashole")
-	// Retornos con nombre
-	fmt.Println(getValues(2))
+	hellomod2.SayHello("Añañin")
+	hellomod.SayHello()
 }
